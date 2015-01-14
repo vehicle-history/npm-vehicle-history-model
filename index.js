868988146776e45eaee02c43290286e0f42cec68
Zenedith <github@zenedith.mailgun.org>
2015-01-14 18:35:58 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var meta = require('./lib/meta');
+
+module.exports = {
+  /**
+   * the CarEngineResolver class.
+   * @property CarEngineResolver
+   * @type CarEngineResolver
+   * @static
+   */
+//  CarEngineResolver: require('./lib/resolver/carEngineResolver'),
+
+  /**
+   * the version of the library
+   * @property VERSION
+   * @type String
+   * @static
+   */
+  VERSION: meta.VERSION
+};
+
+
