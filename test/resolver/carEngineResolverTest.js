6c979a5aef488d9840f1327d540a935a5dd4f544
Zenedith <github@zenedith.mailgun.org>
2015-02-01 19:51:05 +0100

    Added mileage field to inspection event and refactored model

--- test/resolver/carEngineResolverTest.js
+++ test/resolver/carEngineResolverTest.js
@@ -61,10 +61,7 @@ describe('car engine resolver test', function () {
     };
 
     var response = carEngineResolver.resolve(map, options);
-    should.exist(response);
-    should.exist(response.fuel);
-    expect(response.cubicCapacity).to.be.null();
-    response.fuel.should.equal('OTHER');
+    expect(response).to.be.null();
     done();
   });
 
