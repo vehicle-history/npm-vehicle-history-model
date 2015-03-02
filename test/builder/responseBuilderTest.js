d2417d561a09b96e01c30fda1ba9221e92776860
Zenedith <github@zenedith.mailgun.org>
2015-03-02 22:26:57 +0100

    Added some enums

--- test/builder/responseBuilderTest.js
+++ test/builder/responseBuilderTest.js
@@ -96,38 +96,38 @@ describe('response builder test', function () {
     responseBuilder.build(map, options, function (err, response) {
 
       should.exist(response, 'response');
-      should.exist(response.car, 'response.car');
+      should.exist(response.vehicle, 'response.vehicle');
       should.exist(response.events, 'response.events');
-      var car = response.car;
+      var vehicle = response.vehicle;
       var events = response.events;
 
-      expect(car).to.have.deep.property('name.make', 'AUDI');
-      expect(car).to.have.deep.property('name.name', 'name');
-      expect(car).to.have.deep.property('name.model', 'model');
+      expect(vehicle).to.have.deep.property('name.make', 'AUDI');
+      expect(vehicle).to.have.deep.property('name.name', 'name');
+      expect(vehicle).to.have.deep.property('name.model', 'model');
 
-      expect(car).to.have.deep.property('type.type', 'VAN');
-      expect(car).to.have.deep.property('type.kind', 'LIMOUSINE');
+      expect(vehicle).to.have.deep.property('type.type', 'VAN');
+      expect(vehicle).to.have.deep.property('type.kind', 'LIMOUSINE');
 
-      expect(car).to.have.deep.property('engine.cubicCapacity', 1396);
-      expect(car).to.have.deep.property('engine.fuel', 'DIESEL');
+      expect(vehicle).to.have.deep.property('engine.cubicCapacity', 1396);
+      expect(vehicle).to.have.deep.property('engine.fuel', 'DIESEL');
 
-      expect(car).to.have.deep.property('production.year', 1988);
+      expect(vehicle).to.have.deep.property('production.year', 1988);
 
-      expect(car).to.have.deep.property('policy.status', 'OUTDATED');
+      expect(vehicle).to.have.deep.property('policy.status', 'OUTDATED');
 
-      expect(car).to.have.deep.property('registration.status', 'REGISTERED');
-      expect(car).to.have.deep.property('registration.firstAt', '2011-11-21T00:00:00.000Z');
+      expect(vehicle).to.have.deep.property('registration.status', 'REGISTERED');
+      expect(vehicle).to.have.deep.property('registration.firstAt', '2011-11-21T00:00:00.000Z');
 
-      expect(car).to.have.deep.property('inspection.status', 'UPTODATE');
-      expect(car).to.have.deep.property('mileage.value', 111);
-      expect(car).to.have.deep.property('mileage.type', 'MILE');
+      expect(vehicle).to.have.deep.property('inspection.status', 'UPTODATE');
+      expect(vehicle).to.have.deep.property('mileage.value', 111);
+      expect(vehicle).to.have.deep.property('mileage.type', 'MILE');
 
-      expect(car).to.have.property('stolen', true);
+      expect(vehicle).to.have.property('stolen', true);
 
-      expect(car).to.have.deep.property('plate.value', 'AAE 1111');
-      expect(car).to.have.deep.property('plate.country', 'PL');
+      expect(vehicle).to.have.deep.property('plate.value', 'AAE 1111');
+      expect(vehicle).to.have.deep.property('plate.country', 'PL');
 
-      expect(car).to.have.property('vin', 'ABC123456789DEF');
+      expect(vehicle).to.have.property('vin', 'ABC123456789DEF');
 
       expect(events).to.have.deep.property('[0].type', 'PRODUCTION');
 //      expect(events).to.have.deep.property('[0].createdAt', '2012-06-13T00:00:00.000Z');
